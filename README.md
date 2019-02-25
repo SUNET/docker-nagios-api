709d6c543f54702abb14ff5671f9d2c1cd6e81be
Leif Johansson <leifj@sunet.se>
2019-02-25 16:09:19 +0100

    script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
-Look at sunet::nagiosapi for simple usage
+Look at sunet::nagiosapi for simple deploy pattern
+
+Scripts directory has a simple nagios plugin that monitors several nagios instances
+for trends across hosts with similar services.
+
+To use install python-requests and python-nagiosplugin
---
+++ scripts/nagios-service-summary.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import nagiosplugin
+import argparse
+import requests
+
+svc = sys.argv.pop()
+summary = dict()
+
+class SetEncoder(json.JSONEncoder):
+   def default(self, obj):
+      if isinstance(obj, set):
+         return list(obj)
+      return json.JSONEncoder.default(self, obj)
+
+_states = {'0': nagiosplugin.state.Ok,
+           '1': nagiosplugin.state.Warn,
+           '2': nagiosplugin.state.Critical,
+           '3': nagiosplugin.state.Unknown}
+
+class ServiceSummary(nagiosplugin.Resource):
+   def __init__(self, service_name, status_urls):
+      self.service_name = service_name
+      self.status_urls = status_urls
+
+   def probe(self):
+      self.summary = dict()
+      for status_url in self.status_urls:
+         r = requests.get(status_url)
+         if r.ok: 
+            status = r.json()
+            for host,status in status['content'].items():
+               if self.service_name in status['services']:
+                  current_state = _states[str(status['services'][self.service_name]['current_state'])]
+                  self.summary.setdefault(current_state,set())
+                  self.summary[current_state].add(host)
+      for state,hosts in self.summary.items():
+         yield nagiosplugin.Metric('Number of {} hosts for service {}'.format(state,self.service_name),len(hosts),context=self.service_name)
+
+@nagiosplugin.guarded
+def main():
+   argp = argparse.ArgumentParser(description=__doc__)
+   argp.add_argument('-w', '--warning', metavar='RANGE', default='', help='return warning if number of failed hosts is outside RANGE')
+   argp.add_argument('-c', '--critical', metavar='RANGE', default='', help='return critical if number of failed hosts is outside RANGE')
+   argp.add_argument('-s', '--service', metavar='SERVICE NAME', default='Uptime', help='the service to summarize')
+   argp.add_argument('-S', '--status', metavar='JSON STATUS FILE', type=str, action='append', help='<Required> status source')
+   argp.add_argument('-v', '--verbose', action='count', default=0, help='increase output verbosity (use up to 3 times)')
+   args = argp.parse_args()
+   check = nagiosplugin.Check(ServiceSummary(args.service,args.status),
+                              nagiosplugin.ScalarContext(args.service, args.warning, args.critical))
+   check.main(verbose=args.verbose)
+
+if __name__ == '__main__':
+   main()
